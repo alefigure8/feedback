cd1a5c8f73c389dee269d56cd22598a8f59c5a65
ale_figure8 <gomeznieto@gmail.com>
2022-03-08 17:00:13 -0300

    Adding chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
-import { AuthProvider } from '../lib/auth'
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { AuthProvider } from '@/lib/auth'
+import '@/styles/globals.css'
+import theme from '@/styles/theme'
+import { extendTheme } from '@chakra-ui/react'
+
+const personalTheme = extendTheme({theme})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={personalTheme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   )
 }
 
